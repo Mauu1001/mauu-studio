7d34fbd07471c7dfe53fe68c53c4d840f5f470b9
Mauu1001 <mahalaumarius@gmail.com>
2026-06-23 08:31:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 // - tanstackStart, viteReact, tailwindcss, tsConfigPaths, nitro, componentTagger, etc.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
- 
+
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
     target: "vercel",
   },
+  nitro: true,
 });
- 
